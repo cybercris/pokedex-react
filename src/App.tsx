e5b29f25f692d01ca2f60820d23e6acc9e483e74
Cristiano Soares <crssoares.js@gmail.com>
2023-04-13 00:27:19 -0300

    adding tailwindcss for UI Framework + router + queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+import queryClient from './services/queryClient'
 import { Router } from './Router'
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function Home() {
-  return <h1>Home</h1>
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
